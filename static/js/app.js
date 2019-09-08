44e31f905ba62b5cd4105ffbd7c01cf299cee0e0
pinehat <pinehat@gmail.com>
2019-09-07 22:15:12 -0400

    completed

--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,13 @@ console.log(tableData);
 var dataColumns = ["datetime" , "city" , "state" , "country" , "shape" , "durationMinutes" , "comments"];
 
 // Define function to append each row of the data to the webpage table
-function loadData(){
-    tableData.forEach(sighting => {
+function loadData()
+{
+    tableData.forEach(sighting => 
+    {
         var row = tbody.append("tr");
-        dataColumns.forEach(column => {
+        dataColumns.forEach(column => 
+        {
             row.append("td").text(sighting[column]); 
         });
     });
@@ -25,29 +28,29 @@ loadData();
 
 // Store references to user input fields by their ids 
 var inputDate = d3.select("#datetime")
-var inputCity = d3.select("#city")
-var inputState = d3.select("#state")
-var inputCountry = d3.select("#country")
-var inputShape = d3.select("#shape")
+var inputCity = d3.select("#city");
+var inputState = d3.select("#state");
+var inputCountry = d3.select("#country");
+var inputShape = d3.select("#shape");
 
 // Store reference to the Filter Table button by its id
-var filterTable = d3.select("#filter-btn")
+var filterTable = d3.select("#filter-btn");
 
 // Store reference to the Reset Table button by its id
-var resetTable = d3.select("#reset-btn")
+var resetTable = d3.select("#reset-btn");
 
 // Define function to filter the data to display based on user criteria
 function filterData(){
 
     // Don't allow page refresh
-    d3.event.preventDefault()
+    d3.event.preventDefault();
 
     // Store user inputs as variables
-    var userDate = inputDate.property("value")
-    var userCity = inputCity.property("value")
-    var userState = inputState.property("value")
-    var userCounty = inputCountry.property("value")
-    var userShape = inputShape.property("value")
+    var userDate = inputDate.property("value");
+    var userCity = inputCity.property("value");
+    var userState = inputState.property("value");
+    var userCounty = inputCountry.property("value");
+    var userShape = inputShape.property("value");
 
     // Filter the data according to user inputs and store results in a variable
     var filteredData = tableData.filter(filterBy => {
@@ -56,8 +59,8 @@ function filterData(){
                 (filterBy.state === userState || userState == "") &&
                 (filterBy.country === userCounty || userCounty == "") &&
                 (filterBy.shape === userShape || userShape == "")
-            )
-    })
+            );
+    });
 
     // Display filtered data in console
     console.log(filteredData);
@@ -66,21 +69,25 @@ function filterData(){
     tbody.text("");
 
     // Repopulate table with user filtered data    
-    filteredData.forEach(sighting => {
-        var row = tbody.append("tr")
-        dataColumns.forEach(column => {
-            row.append("td").text(sighting[column])    
+    filteredData.forEach(sighting => 
+        {
+        var row = tbody.append("tr");
+        dataColumns.forEach(column => 
+            {
+            row.append("td").text(sighting[column]);  
+            })
         })
-    })
 }
+
 // Run filterData function when user clicks Filter Table button
 filterTable.on("click", filterData);
 
 // Define function to re-display original full dataset in table 
-function resetData(){
-    tbody.text("")
-    loadData()
-    }
+function resetData()
+    {
+    tbody.text("");
+    loadData();
+    };
     
 // Run resetData function when user clicks Reset Table button
 resetTable.on("click", resetData);
